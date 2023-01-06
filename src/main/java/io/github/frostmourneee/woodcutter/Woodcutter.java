22943ab2fd9e601a53a25df96da7fabe238e0df8
Frostmourneee <dolpad@yandex.ru>
2023-01-06 20:28:14 +0300

    Initial commit

---
+++ src/main/java/io/github/frostmourneee/woodcutter/Woodcutter.java
@@ -0,0 +1,9 @@
+package io.github.frostmourneee.woodcutter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Woodcutter.MODID)
+public class Woodcutter {
+
+    public static final String MODID = "woodcutter";
+}
